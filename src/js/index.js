275cdc7ea442657eae50bb9d48af9c791351f845
elvvira <elvvira.castellanos@gmail.com>
2023-02-02 23:55:56 +0100

    casa

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,8 @@ import '../scss/styles.scss';
 
 const select = document.getElementById('select');
 const containerCards = document.getElementById('container-cards');
-// let region;
+const inputSearch = document.getElementById('input');
+const cardElement = document.getElementsByClassName('.card');
 
 const fetchData = async url => {
   const response = await fetch(url);
@@ -14,16 +15,17 @@ const fetchData = async url => {
 const createCards = array => {
   containerCards.innerHTML = '';
   const fragment = document.createDocumentFragment();
+
   array.forEach(country => {
     const countryName = country.name.common;
 
-    const newElement = document.createElement('div');
-    newElement.classList.add('card');
-    fragment.append(newElement);
+    const cardElement = document.createElement('div');
+    cardElement.classList.add('card');
+    fragment.append(cardElement);
 
     const imageContainer = document.createElement('div');
     imageContainer.classList.add('card__container');
-    newElement.append(imageContainer);
+    cardElement.append(imageContainer);
 
     const imgElement = document.createElement('img');
     imgElement.classList.add('card__image');
@@ -32,7 +34,7 @@ const createCards = array => {
 
     const cardInformation = document.createElement('div');
     cardInformation.classList.add('card__information');
-    newElement.append(cardInformation);
+    cardElement.append(cardInformation);
 
     const h2Element = document.createElement('h2');
     h2Element.textContent = countryName;
@@ -47,12 +49,23 @@ const createCards = array => {
     const pElement3 = document.createElement('p');
     pElement3.textContent = `Capital: ${country.capital}`;
     cardInformation.append(pElement3);
-
-    console.log(country.flags.png);
   });
 
   containerCards.append(fragment);
+  // cardWindow();
 };
+
+// const cardWindow = () => {};
+
+// cardElement.addEventListener('click', e => {
+//   console.log(e.target.value);
+// });
+
+inputSearch.addEventListener('change', e => {
+  const country = e.target.value;
+
+  fetchData(`https://restcountries.com/v3.1/name/${country}`);
+});
 
 select.addEventListener('change', e => {
   const region = e.target.value;
