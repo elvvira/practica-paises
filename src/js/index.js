de80507295740a2e6f6b0a40ae3d0fa2a1e271b1
Daniel Jiménez <elvvira.castellanos@gmail.com>
2023-02-02 14:59:41 +0100

    clase

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,60 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
-import { sayHello } from './demo.js';
 
-sayHello();
+const select = document.getElementById('select');
+const containerCards = document.getElementById('container-cards');
+// let region;
+
+const fetchData = async url => {
+  const response = await fetch(url);
+  const data = await response.json();
+  createCards(data);
+};
+
+const createCards = array => {
+  containerCards.innerHTML = '';
+  const fragment = document.createDocumentFragment();
+  array.forEach(country => {
+    const countryName = country.name.common;
+
+    const newElement = document.createElement('div');
+    newElement.classList.add('card');
+    fragment.append(newElement);
+
+    const imageContainer = document.createElement('div');
+    imageContainer.classList.add('card__container');
+    newElement.append(imageContainer);
+
+    const imgElement = document.createElement('img');
+    imgElement.classList.add('card__image');
+    imgElement.src = country.flags.png;
+    imageContainer.append(imgElement);
+
+    const cardInformation = document.createElement('div');
+    cardInformation.classList.add('card__information');
+    newElement.append(cardInformation);
+
+    const h2Element = document.createElement('h2');
+    h2Element.textContent = countryName;
+    cardInformation.append(h2Element);
+
+    const pElement = document.createElement('p');
+    pElement.textContent = `Population:${country.population}`;
+    cardInformation.append(pElement);
+    const pElement2 = document.createElement('p');
+    pElement2.textContent = `Region: ${country.region}`;
+    cardInformation.append(pElement2);
+    const pElement3 = document.createElement('p');
+    pElement3.textContent = `Capital: ${country.capital}`;
+    cardInformation.append(pElement3);
+
+    console.log(country.flags.png);
+  });
+
+  containerCards.append(fragment);
+};
+
+select.addEventListener('change', e => {
+  const region = e.target.value;
+  fetchData(`https://restcountries.com/v3.1/region/${region}`);
+});
